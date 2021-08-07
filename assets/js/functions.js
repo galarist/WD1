5ec365ebef6bd95292fc983733167f331cce0631
Cristovao Galambos <galamboscristovao@gmail.com>
2021-08-08 00:09:41 +1000

    navbar has been changed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-$('#menuContainer a').click(function (e) {
-    $('a').removeClass('active'); // Removing class from the link that has been clicked
+$('nav a').click(function (e) {
+    $('nav a').removeClass('active'); // Removing class from the link that has been clicked
     $(this).addClass('active'); // Adding class to the link that has been clicked
 });
